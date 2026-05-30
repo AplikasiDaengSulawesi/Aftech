5892b6e267f377e9906cd65ef69b2658a1eef32d
rahmat999ht <rahmat999ht@gmail.com>
2026-05-30 16:12:02 +0800

    ubah ukuran surat jalan

--- pages/print_surat_jalan.php
+++ pages/print_surat_jalan.php
@@ -60,8 +60,8 @@ $tanggal_cetak = date('d-m-Y H:i');
     <title>Surat Jalan <?php echo htmlspecialchars($header['surat_jalan_no']); ?></title>
 <style>
     @page {
-        size: 5.5in 8.3in;
-        margin: 0.3in 0.1in
+        size: 8.5in 5.5in;
+        margin: 0.3in 0.1in;
     }
 
     * { box-sizing: border-box; }
